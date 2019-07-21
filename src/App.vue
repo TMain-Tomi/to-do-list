7cbb5c5d11c863e67ba1d793752ccb9df80c5274
MrLinZHui <742847492@qq.com>
2019-07-21 21:51:58 +0800

    finish head and foot

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" style="">
+    <AddCheck/>
+  </div>
+</template>
+
+<script>
+import AddCheck from './components/AddCheck.vue'
+export default {
+  name: 'app',
+  components: {
+    AddCheck
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/style.css";
+</style>
---
+++ src/components/AddCheck.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Card class="div-card">
+      <div class="div-header">
+        <div class="div-header-title">
+          <h1>Jquery To Do List</h1>
+        </div>
+        <div class="div-header-comment">
+          <h3><i>Simple Todo List with adding and filter by diff status.</i></h3>
+        </div>
+      </div>
+     
+      <div class="div-footer">
+        <Button size="large" class="div-footer-button" @click="filterItems(1)">All</Button>
+        <Button size="large" class="div-footer-button" @click="filterItems(2)">Active</Button>
+        <Button size="large" class="div-footer-button-complete" @click="filterItems(3)">Complete</Button>
+      </div>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddCheck',
+  data () {
+    return {
+      single: false,
+      items: [],
+      itemsByStatus: [],
+      itemName: '',
+      tableStatus: 1,
+    }
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
